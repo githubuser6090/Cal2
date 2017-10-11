55af24e65983cf4f0580dca041f9445f43c57264
Student <Student@ZAPRE-D00000178.DirectDelivery.com>
2017-10-11 11:04:16 +0200

    CalculationProject1

---
+++ src/cal2/Cal2.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cal2;
+
+/**
+ *
+ * @author Student
+ */
+public class Cal2 {
+
+    public int Add (String s1, String s2){
+        return 0;
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
